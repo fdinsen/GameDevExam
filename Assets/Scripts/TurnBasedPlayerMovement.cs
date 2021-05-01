da0f01932b9332ae5ffb25d78bbcf144f55f8a74
Frederik <gammarik@gmail.com>
2021-05-01 13:13:02 +0200

    Adds player movement

--- Assets/Scripts/TurnBasedPlayerMovement.cs
+++ Assets/Scripts/TurnBasedPlayerMovement.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 
 public class TurnBasedPlayerMovement : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] [Tooltip("The distance in units with which the player moves.")]
     private float _moveDistance = 0.5f;
     [SerializeField] [Tooltip("The speed with which the player moves from square to square.")]
     private float _moveSpeed = 5f;
-    [SerializeField] [Tooltip("Local Empty Gameobject that defines the next move-point.")]
-    private Transform _movePoint;
+    [SerializeField] [Tooltip("The speed with which the player rotates when moving from square to square")]
+    private float _rotationSpeed = 5f;
+    [SerializeField] [Range(0, 1)]
+    [Tooltip("The time, in seconds, between inputs being registred when holding down a move key.")]
+    private float _timeBetweenInputs = .1f;
     [SerializeField] [Tooltip("Which layers that should stop movement.")]
     private LayerMask _whatStopsMovement;
-    [SerializeField] [Range(0,1)] [Tooltip("The time, in seconds, between inputs being registred when holding down a move key.")] 
-    private float _timeBetweenInputs = .1f;
+
+    [Header("Object references")]
+    [SerializeField] [Tooltip("Local Empty Gameobject that defines the next move-point.")]
+    private Transform _movePoint;
+    [SerializeField] [Tooltip("Local Camera")]
+    private Transform _camera;
 
     public static event Action PlayerMoved;
 
@@ -23,8 +31,9 @@ public class TurnBasedPlayerMovement : MonoBehaviour
     private Animator _animator;
     private void Start()
     {
-        //makes sure the movepoint won't move with the player
+        //makes sure the movepoint and camera won't move with the player
         _movePoint.parent = null;
+        _camera.parent = null;
         _moveCooldown = _timeBetweenInputs;
         _animator = GetComponent<Animator>();
     }
@@ -51,6 +60,7 @@ public class TurnBasedPlayerMovement : MonoBehaviour
     public void Move()
     {
         transform.position = Vector3.MoveTowards(transform.position, _movePoint.position, _moveSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, _movePoint.rotation, _rotationSpeed * Time.deltaTime);
 
         if(_moveCooldown <= 0) 
         {
@@ -63,9 +73,9 @@ public class TurnBasedPlayerMovement : MonoBehaviour
                     if(CheckCollision(_movePoint.position, new Vector3(input.x * _moveDistance, 0f, 0f)) )
                     {
                         _movePoint.position += new Vector3(input.x * _moveDistance, 0f, 0f);
+                        _movePoint.rotation = Quaternion.Euler(0, 90 * input.x, 0);
                         PlayerMoved?.Invoke();
                         _animator?.SetFloat("MoveSpeed", Math.Abs(input.x));
-                        transform.rotation = Quaternion.Euler(0, 90*input.x, 0);
                     }
                 }
                 if (Mathf.Abs(input.y) == 1f)
@@ -73,9 +83,9 @@ public class TurnBasedPlayerMovement : MonoBehaviour
                     if (CheckCollision(_movePoint.position, new Vector3(0f, 0f, input.y * _moveDistance)) )
                     {
                         _movePoint.position += new Vector3(0f, 0f, input.y * _moveDistance);
+                        _movePoint.rotation = Quaternion.Euler(0, 180 * Mathf.Clamp(input.y * 2, 1, 2), 0);
                         PlayerMoved?.Invoke();
                         _animator?.SetFloat("MoveSpeed", Math.Abs(input.y));
-                        transform.rotation = Quaternion.Euler(0, 180 * Mathf.Clamp(input.y *2, 1,2), 0);
                     }
                 }
             }
